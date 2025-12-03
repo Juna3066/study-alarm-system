8fefb1de9655da8b717e63dc853b139a66e33127
juna3066 <juna3066@qq.com>
2025-12-03 13:22:09 +0800

    feat: Adjust main window dimensions and behavior

--- get-portable-app/main.js
+++ get-portable-app/main.js
@@ -5,10 +5,14 @@ let win;
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 1280,
-        height: 800,
-        minWidth: 1024,
-        minHeight: 768,
+        width: 1200,
+        height: 1000,
+        maxWidth: 1200,
+        maxHeight: 1000,
+        minWidth: 390,
+        minHeight: 700,
+        resizable: true,
+        maximizable: false,
         title: '校园闹铃系统',
         icon: path.join(__dirname, 'external-resources', 'icon.ico'),
         webPreferences: {
